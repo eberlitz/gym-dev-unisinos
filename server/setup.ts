5a3561cd15cd2c5465a857fbcaf5e9d216a6f4cc
Douglas Eccker <douglaseccker@hotmail.com>
2017-09-23 16:18:32 -0300

    fix error creating admin user

--- server/setup.ts
+++ server/setup.ts
@@ -1,21 +1,20 @@
 import { User } from './models/user';
 import mongoose = require('mongoose');
 
-
-
 async function init() {
   let adminUser = await User.findById('59ac3b3736adff29536e9ed9');
+
   if (!adminUser) {
     adminUser = new User({
       _id: new mongoose.Types.ObjectId('59ac3b3736adff29536e9ed9'),
       local: {
         username: 'admin'
-      }
+      },
+      name: 'Admin'
     });
     adminUser.local.password = adminUser.generateHash('admin');
   }
   return await adminUser.save();
 }
-
 
 init();
